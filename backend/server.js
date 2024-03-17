b4161a6adaf63e9dc0b90a934bcc7280080eace3
satish99bkp <satish99bkp@gmail.com>
2024-03-17 12:01:57 +0530

    firt part added - Backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+
+
+const app=express();
+const PORT=process.env.PORT||5000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+// app.get("/", (req, res)=>{
+// //    root route http://localhost:5000/
+// res.send("Hello World!");
+// });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, ()=> {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
