65c12a99e275cc623b76427b1df8231326212688
satish99bkp <satish99bkp@gmail.com>
2024-03-23 20:25:22 +0530

    fourth commit

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId } from "../socket/socket.js";
+import { io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -27,6 +29,11 @@ export const sendMessage = async (req, res) => {
     // await conversation.save();
     // await newMessage.save();
     await Promise.all([conversation.save(), newMessage.save()]);
+    const receiverSocketId=getReceiverSocketId(receiverId);
+    if(receiverSocketId){
+      io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller:", error.message);
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,12 +3,13 @@ import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
 import { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
-  useEffect(()=>{
-    return ()=> setSelectedConversation(null);
+  useEffect(() => {
+    return () => setSelectedConversation(null);
   }, [setSelectedConversation]);
   return (
     <div className="md:min-w-[450px] flex flex-col">
@@ -18,7 +19,9 @@ const MessageContainer = () => {
         <>
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:</span>
-            <span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
           </div>
           <Messages />
           <MessageInput />
@@ -30,13 +33,14 @@ const MessageContainer = () => {
 
 export default MessageContainer;
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div
         className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col
             items-center gap-2"
       >
-        <p>Welcome 👏 Satish Singh ✨</p>
+        <p>Welcome 👏 {authUser.fullName} ✨</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
